a9f94be351d6fd2d0d6381d3676b4a9e0d8fc003
tupeajay523 <tupeajay523@gmail.com>
2024-08-06 23:47:48 +0530

    Day1/Q4

---
+++ Day1/Q4.py
@@ -0,0 +1,13 @@
+#Q4. Write a Python program that takes an integer input from the user and prints whether the number is
+# positive, negative, or zero.
+
+
+
+num = int(input("Enter an integer number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
