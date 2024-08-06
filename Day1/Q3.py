9acd9e3a7d38151f008f58c25be9043aaf61ac2b
tupeajay523 <tupeajay523@gmail.com>
2024-08-06 23:17:03 +0530

    Day1/Q3

---
+++ Day1/Q3.py
@@ -0,0 +1,6 @@
+# Q3 Add comments to your code explaining what each line does.
+
+name = "XYZ" # Assign name to the name variable
+age = 25   # Assign age to the age variable
+favorite_hobby = "ABC"  # Assign hobby to the favorite_hobby
+
