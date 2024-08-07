1604634b5203a4637b757575e223f5dbd35abeb7
tupeajay523 <tupeajay523@gmail.com>
2024-08-07 18:41:22 +0530

    cosmatic changes

--- Day3/Datetime.py
+++ Day3/Datetime.py
@@ -2,10 +2,10 @@
 
 from datetime import datetime, timedelta
 def generate_dates(start_date, end_date):
-    current_date = start_date
-    while current_date <= end_date:
-        yield current_date.strftime("%Y-%m-%d")
-        current_date += timedelta(days=1)
+    curr_date = start_date
+    while curr_date <= end_date:
+        yield curr_date.strftime("%Y-%m-%d")
+        curr_date += timedelta(days=1)
 start_date = datetime(2024, 5, 1)
 end_date = datetime(2024, 6, 30)
 
